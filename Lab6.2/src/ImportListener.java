d8de6eb967340a95a5294bd110faac23ad78d4bb
Hououin Kyoma <kjkszpj361@gmail.com>
2017-04-26 18:13:57 +0300

    Удалил всю ненужную хрень, теперь буду складывать сюда цельные проекты из идеи, тут исправлен импорт, добавлено немного многопоточности

---
+++ Lab6.2/src/ImportListener.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Денис on 25.04.2017.
+ */
+public class ImportListener extends LabListener {
+    ImportListener(JTextField field, TreeSet<Human> col, LabTable colTable,JTextPane out){
+        super(field,col,colTable,out);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                Matcher m= Pattern.compile("\\{([^}]+)\\}").matcher(getObject().getText());
+                while (m.find()) {
+                    getCollection().addAll(ConsoleApp.ImportFrom(m.group().substring(1, m.group().length() - 1)).getUselessData());
+                    getColTable().fireTableDataChanged();
+                }
+            }
+        }).start();
+    }
+}
--- ProjectAlex/src/Console_Gui.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Денис on 24.04.2017.
- */
-public class Console_Gui {
-
-}
