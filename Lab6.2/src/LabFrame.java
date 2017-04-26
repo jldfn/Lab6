d6b4904eecf4a6974d02a614554f5e4c9d7f70de
Hououin Kyoma <kjkszpj361@gmail.com>
2017-04-26 19:30:13 +0300

    Добавлен фильтр по столбцам

--- Lab6.2/src/LabFrame.java
+++ Lab6.2/src/LabFrame.java
@@ -3,6 +3,8 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.TreeSet;
 
 /**
@@ -29,8 +31,26 @@ public class LabFrame extends JFrame {
         LabButton rmLButton=new LabButton("Remove Lower",labCollection.getUselessData(),table,"RmL",OutputPanel);
         LabButton ImportButton=new LabButton("Import",labCollection.getUselessData(),table,"Imp",OutputPanel);
         JButton saveButton=new JButton("Save");
-        RowSorter<LabTable> sort=new TableRowSorter<LabTable>(table);
+        TableRowSorter<LabTable> sort=new TableRowSorter<LabTable>(table);
         JTable sortTable=new JTable(table);
+        JTextField filterText=new JTextField(30);
+        JButton filterButton=new JButton("Filter");
+        JPanel filterPanel=new JPanel();
+        filterPanel.add(filterButton);
+        filterPanel.add(filterText);
+        JScrollPane tPanel=new JScrollPane(sortTable);
+        JPanel TablePanel= new JPanel();
+        TablePanel.add(filterPanel);
+        TablePanel.add(tPanel);
+        filterButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(filterText.getText().length()==0){sort.setRowFilter(null);}
+                else{
+                    sort.setRowFilter(RowFilter.regexFilter(filterText.getText()));
+                }
+            }
+        });
         sortTable.setRowSorter(sort);
         saveButton.addActionListener(new SaveListener(labCollection,OutputPanel));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -41,7 +61,7 @@ public class LabFrame extends JFrame {
         add(rmButton.getButtonPanel());
         add(saveButton);
         add(OutputPanel);
-        add(new JScrollPane(sortTable));
+        add(TablePanel);
         pack();
     }
 
